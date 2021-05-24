7062f8cf2d80b83c2e3a1858f5a86beb5efa68c5
shinjw <cobalt41g@gmail.com>
2021-05-24 19:47:52 +0900

    djust django 14

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
