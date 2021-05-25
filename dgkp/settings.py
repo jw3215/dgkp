ffe3349d39dd614b7de351039ff08088fd2123cc
shinjw <cobalt41g@gmail.com>
2021-05-25 23:26:10 +0900

    djust django 29

--- dgkp/settings.py
+++ dgkp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.forms.widgets import Media
 import environ
 from django.urls.base import reverse_lazy
 
@@ -141,3 +142,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = reverse_lazy("accountapp:hello_world")
 LOGOUT_REDIRECT_URL = reverse_lazy("accountapp:login")
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
